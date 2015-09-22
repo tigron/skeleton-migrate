f01d705cf10f214acd3b67292aab03846e0c9cf3
Tigron <tigron@bookkeeping.tigron.net>
2015-09-22 11:39:57 +0200

    first version of migration

---
+++ lib/Skeleton/Database/Migration/Config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Config class
+ * Configuration for Skeleton\Database\Migration
+ *
+ * @author Christophe Gosiau <christophe@tigron.be>
+ * @author Gerry Demaret <gerry@tigron.be>
+ */
+
+namespace Skeleton\Database\Migration;
+
+class Config {
+
+	/**
+	 * The migration directory
+	 *
+	 * @access public
+	 * @var string $migration_directory
+	 */
+	public static $migration_directory = null;
+}
