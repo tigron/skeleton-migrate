7f1602b90f460445f6364d8dd97b3a4fc136587a
Tigron <tigron@bookkeeping.tigron.net>
2015-09-22 16:18:47 +0200

    migrate up add console command

---
+++ console/up.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * migration:create command for Skeleton Console
+ *
+ * @author Gerry Demaret <gerry@tigron.be>
+ * @author Christophe Gosiau <christophe@tigron.be>
+ * @author David Vandemaele <david@tigron.be>
+ */
+
+namespace Skeleton\Console\Command;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Migrate_Up extends \Skeleton\Console\Command {
+
+	/**
+	 * Configure the Create command
+	 *
+	 * @access protected
+	 */
+	protected function configure() {
+		$this->setName('migrate:up');
+		$this->setDescription('Update the database with all existing migrations');
+	}
+
+	/**
+	 * Execute the Command
+	 *
+	 * @access protected
+	 * @param InputInterface $input
+	 * @param OutputInterface $output
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		if (!file_exists(\Skeleton\Database\Migration\Config::$migration_directory)) {
+			throw new \Exception('Config::$migration_directory is not set to a valid directory');
+		}
+		$output->writeln(\Skeleton\Database\Migration\Runner::up());
+		$output->writeln('Database up-to-date' );
+	}
+}
---
+++ template/migration.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Database migration class
+ *
+ * @author Gerry Demaret <gerry@tigron.be>
+ * @author Christophe Gosiau <christophe@tigron.be>
+ * @author David Vandemaele <david@tigron.be>
+ */
+
+class %%classname%% extends \Skeleton\Database\Migration {
+
+	/**
+	 * Migrate up
+	 *
+	 * @access public
+	 */
+	public function up() {
+
+	}
+
+	/**
+	 * Migrate down
+	 *
+	 * @access public
+	 */
+	public function down() {
+
+	}
+}
